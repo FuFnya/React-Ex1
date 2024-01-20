1599c95d882e08d0fad998e81eea2d7f5f12a162
Philip Shlifer <ben13ph@gmail.com>
2024-01-20 16:06:12 +0200

    fix

--- ex2.js
+++ ex2.js
@@ -1,73 +1,69 @@
+class Duck {
+  constructor(name, color, age, weight, pic) {
+    this.name = name;
+    this.color = color;
+    this.age = age;
+    this.weight = weight;
+    this.pic = pic;
+  }
 
+  Show() {
+    return (
+      "Duck info: " +
+      this.name +
+      " " +
+      this.color +
+      " " +
+      this.age +
+      " " +
+      this.weight +
+      " " +
+      this.pic
+    );
+  }
 
-// class Duck {
-//     constructor(name, color, age, weight, pic) {
-//         this.name = name
-//         this.color = color
-//         this.age = age
-//         this.weight = weight
-//         this.pic = pic
-//     }
+  Quack() {
+    const audio = new Audio("../quack.mp3");
+    playSound("../quack.mp3", 3)
+    let timesToQuack = (this.age * this.weight) / 2;
+    return  "Quack! ".repeat(timesToQuack);
+  }
+}
 
-//     Show () {
-//         return {"Name": this.name, "Color": this.color, "Age": this.age, "Weight": this.weight, "Pic": this.pic}
-//     }
+const playSound = (audioURL, playCount = 1) => {
 
-//     Quack() {
-//         quacks = []
-//         for (var i = 0; i < (this.weight*this.age)/2; i++){
-//             quacks[i] = "Quack"
-//         }
-//         for (var i = 0; i < 3; i++){
-//             audio.play()
-//         }
-//         return quacks
-//     }
+  const audioContext = new AudioContext();   
+  audioContext.resume();
 
-// }
+  const audioClip = new Audio(audioURL);
 
-// const audio = new Audio("quack.mp3");
-// //audio.play()
-// var duck = null
+  const audioClipDuration = 2000;
+  audioClip.loop = true;
+  setTimeout(() => audioClip.loop = false, (audioClipDuration * (playCount - 1)));
+  audioClip.play();
+}
 
-// // $(document).ready(function(e) {
-// //     $("#form").on("submit", check)
-// // })
+$(document).ready(() => {
+  $("#myForm").submit((event) => {
+    event.preventDefault(); // Prevent the form from submitting normally
 
-// $(function(e) {
-//     $("#form").on("submit", check)
-// })
+    Submitted();
+  });
+});
 
-// function check(){
-//     duck = new Duck(
-//         $("#name").val(),
-//         $("#color").val(),
-//         $("#age").val(),
-//         $("#weight").val(),
-//         $("#pic").val()
-//     )
-//     // $("body").append("")
-//     return false
-// }
+function Submitted() {
+  myDuck = new Duck( $("#name").val(), $("#color").val(), $("#age").val(), $("#weight").val(), $("#pic").val() );
 
-// function show(){
-//     for ([x,y] of Object.entries(duck.Show())) {
-//         console.log(x,y)
-//     }
-// }
-// function quack(){
-//     for (x of duck.Quack()) {
-//         console.log(x)
-//     }
-// }
+  $("#subBtn").prop("disabled", true);
+  console.log(Duck);
+  $("#hiddenDiv").css("visibility", "visible");
+}
+function showDuck() {
+  $("#dispDiv").html("");
+  $("#dispDiv").html(myDuck.Show());
+}
 
-// // $(document).on('submit',function (e) {
-// //     $("#form").append("aaaaa")
-// //     return
-// // });
-
-
-const audio = new Audio("quack.mp3");
-function quack(){
-        audio.play()
+function Quack(){
+  $("#dispDiv").html("");
+  $("#dispDiv").html(myDuck.Quack());
 }
